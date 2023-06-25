b6cbefb96e0d7e179bc8718d0be1676c3e4f2f89
itachi-P <itachip38@gmail.com>
2023-06-25 18:52:45 +0900

    Separated for console and browser.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # My first introduction to typescripts
 
 ## Learning Policy
-Once I have some proficiency in Golang, resume learning TypeScript.<br>
-After that, I plans to try React.js, Next.js, and Tailwind CSS.<br>
+
+Once I have some proficiency in Golang, resume learning TypeScript.
+After that, I plans to try React.js, Next.js, and Tailwind CSS.
+
+## Notice
+
+※基本的にコンソール上で動作するものとブラウザ上で動作するコードは混ぜない
+ブラウザ上で動作するものだけで纏めてディレクトリ構成を分けることにする
--- diceroll.ts
+++ diceroll.ts
@@ -1,33 +1,36 @@
+//ローカルのNode.jsを使う前提。ブラウザ上では動かない
+// Usage: `$ node diceroll.ts 10`
+
 // サイコロを振る関数
 function rollDice(): number {
-  return Math.floor(Math.random() * 6) + 1;
+	return Math.floor(Math.random() * 6) + 1;
 }
 
-// メインの処理
 function main() {
-  const args = process.argv.slice(2);
-  if (args.length !== 1) {
-    console.log('正しい引数を指定してください。');
-    console.log('使用法: node script.js <回数>');
-    process.exit(1);
-  }
+	//processはNode.jsのグローバル変数
+	const args = process.argv.slice(2);
+	if (args.length !== 1) {
+		console.log('正しい引数を指定してください。');
+		console.log('使用法: node script.js <回数>');
+		process.exit(1);
+	}
 
-  const count = parseInt(args[0], 10);
-  if (isNaN(count) || count <= 0) {
-    console.log('回数には正の整数を指定してください。');
-    process.exit(1);
-  }
+	const count = parseInt(args[0], 10);
+	if (isNaN(count) || count <= 0) {
+		console.log('回数には正の整数を指定してください。');
+		process.exit(1);
+	}
 
-  console.log(`${count}回サイコロを振った結果:`);
-  let sum = 0;
-  for (let i = 0; i < count; i++) {
-    const result = rollDice();
-    sum += result;
-    console.log(`投げた回数: ${i + 1}, 出目: ${result}`);
-  }
+	console.log(`${count}回サイコロを振った結果:`);
+	let sum = 0;
+	for (let i = 0; i < count; i++) {
+		const result = rollDice();
+		sum += result;
+		console.log(`投げた回数: ${i + 1}, 出目: ${result}`);
+	}
 
-  const average = sum / count;
-  console.log(`合計値: ${sum}, 平均値: ${average.toFixed(2)}`);
+	const average = sum / count;
+	console.log(`合計値: ${sum}, 平均値: ${average.toFixed(2)}`);
 }
 
 // メイン処理を実行
