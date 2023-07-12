6f0dd665d554d360f823d7adf35ccfa7206174a6
itachi-P <itachip38@gmail.com>
2023-07-12 21:18:44 +0900

    update: getServerSidePropsを使い、サーバ側で初期画像を取得するよう変更
    クライアントサイドで最初の画像を取得しレンダリング(CSR)していたのをSSRに変更

--- tutorials/1_making/random-cat/pages/index.tsx
+++ tutorials/1_making/random-cat/pages/index.tsx
@@ -1,27 +1,25 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
 //[https://typescriptbook.jp/tutorials/nextjs]
 
-const IndexPage: NextPage = () => {
-	//猫画像のURLを保持するためにuseStateを使う。初期値は空文字列
-	const [imageUrl, setImageUrl] = useState("");
-	//API呼び出し中かどうかを管理する。初期値は呼び出し中を意味するtrue
-	const [loading, setLoading] = useState(true);
+/* クライアントサイドで最初の画像を取得し表示していたのを、
+データフェッチAPIのgetServerSidePropsを使って、
+サーバーサイドで初期画像を取得するように変更 */
 
-	//コンポーネントがマウントされた時にAPIから画像情報を取得する
-	//ReactのuseEffect関数:第1引数は処理内容、第2引数はどのタイミングで処理を実行するかを指定
-	useEffect(() => { //useEffectには直接非同期関数を指定できないので、async/awaitは使えない
-		//　fetchImageは非同期関数なので、thenメソッドで処理を繋げる
-		fetchImage().then((newImage) => {
-			setImageUrl(newImage.url);//画像URLの状態を更新
-			setLoading(false);//ローディング状態を更新
-		});
-	}, []);//第2引数に空配列を指定することで、コンポーネントがマウントされた時のみ実行される
+// getServerSidePropsから渡されるpropsの型定義
+type Props = {
+	initialImageUrl: string; //初期画像のURL
+};
+
+// ページコンポーネント関数にpropsを受け取る引数を追加
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+	const [imageUrl, setImageUrl] = useState(initialImageUrl);//初期値を渡す
+	const [loading, setLoading] = useState(false);//初期状態はfalseにしておく
 
 	//ボタンをクリックした時に新しい画像を読み込む処理
 	const handleClick = async () => {
-		setLoading(true);//読み込み中フラグを立てる
+		setLoading(true);
 		const newImage = await fetchImage();
 		setImageUrl(newImage.url);
 		setLoading(false);
@@ -35,18 +33,22 @@ const IndexPage: NextPage = () => {
 };
 export default IndexPage;
 
-//下のfetchImage関数の戻り値の型を定義(any型でなく制限をかける)
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+	const image = await fetchImage();
+	return {
+		props: {
+			initialImageUrl: image.url,
+		},
+	};
+};
+
 type Image = {
 	url: string;
 };
 
 //The Cat APIにリクエストし、猫画像を取得する関数
-//fetch及びres.json()は非同期処理なので待機の為にasync/awaitを使う
-/*クライアントサイドが期待するデータ構造をサーバーサイドが必ず返すことを前提とした書き方
-本来であれば、より防衛的にサーバーから返ってくるデータ構造をチェックする必要がある
-厳密にレスポンスをチェックする場合は、unknown型や型ガード関数を使う*/
 const fetchImage = async (): Promise<Image> => {
-	//fetchはHTTPリクエストでリソース(Responseオブジェクト)を取得するブラウザ標準のAPI
 	const res = await fetch("https://api.thecatapi.com/v1/images/search");
 	const images = await res.json();
 	console.log(images);
